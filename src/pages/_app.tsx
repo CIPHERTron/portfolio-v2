86b429800fdd603c3eab81f648e5ecacfdad52ad
CIPHERTron <pritish.samal918@gmail.com>
2022-02-11 01:44:22 +0530

    feat: integrate gogle analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { EmotionCache } from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "@fontsource/recursive/latin.css";
 import "@fontsource/catamaran/latin.css";
 
@@ -28,6 +29,22 @@ const MyApp = ({
         <DefaultSeo {...defaultSEOConfig} />
         <Layout>
           <Component {...pageProps} />
+
+          <Script
+            strategy="lazyOnload"
+            src="https://www.googletagmanager.com/gtag/js?id=G-6R37NRLNJE"
+          />
+
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){
+            dataLayer.push(arguments);            
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-6R37NRLNJE');
+        `}
+          </Script>
         </Layout>
       </ChakraProvider>
     </CacheProvider>

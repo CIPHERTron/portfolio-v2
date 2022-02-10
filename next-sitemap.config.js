dcdc76cda2f0415511ed9d14ccb0b89fdfe74596
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:24:43 +0530

    chore: create next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// https://github.com/iamvishnusankar/next-sitemap#configuration-options
+
+/** @type {import('next-sitemap').IConfig} */
+const NextSitemapConfig = {
+  siteUrl: "https://pritishsamal.tech",
+  generateRobotsTxt: true,
+};
+
+module.exports = NextSitemapConfig;

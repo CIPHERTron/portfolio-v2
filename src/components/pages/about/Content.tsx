8d21feefcde78e947150ceccb141c1e006a5729a
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:29:20 +0530

    feat: add about section

---
+++ src/components/pages/about/Content.tsx
@@ -0,0 +1,50 @@
+import type { BoxProps } from "@chakra-ui/react";
+import { Heading, Link, Text } from "@chakra-ui/react";
+
+import MotionBox from "components/motion/MotionBox";
+import { trackEventToUmami } from "utils/trackEvent";
+
+const textProps: BoxProps = {
+  marginY: 4,
+};
+
+const Content = () => {
+  const handleClickInstagramLink = () => {
+    trackEventToUmami("About: Click Instagram Link", "link");
+  };
+
+  return (
+    <MotionBox
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Heading size="lg">👋 Hi, how are you?</Heading>
+
+      <Text {...textProps}>
+        I&apos;m Pritish, currently a junior year stydent at National Institute
+        of Technology, Rourkela. Most of the time I work with React - TypeScript
+        and Cloud native Technologies (recently). Sometimes I make fun projects
+        and share it here.
+      </Text>
+
+      <Text {...textProps}>
+        Outside working, I like to travel and{" "}
+        <Link
+          href="https://instagram.com/pritish__007"
+          isExternal
+          onClick={handleClickInstagramLink}
+        >
+          <Text as="span" fontWeight="extrabold">
+            capture moments.
+          </Text>
+        </Link>
+      </Text>
+    </MotionBox>
+  );
+};
+
+export default Content;
---
+++ src/components/pages/about/HeadSection.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const HeadSection = () => {
+  return (
+    <Box marginBottom={16}>
+      <Heading size="2xl">CIPHERTron&apos;s Hood</Heading>
+    </Box>
+  );
+};
+
+export default HeadSection;

80e20dfadfa544483d3746453f7561312ecf6eca
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:32:47 +0530

    chore: next seo config

---
+++ src/config/next-seo.ts
@@ -0,0 +1,26 @@
+import type { DefaultSeoProps } from "next-seo";
+
+// https://github.com/garmeeh/next-seo#nextseo-options
+export const defaultSEOConfig: DefaultSeoProps = {
+  title: "Pritish Samal",
+  titleTemplate: "%s | CIPHERTron",
+  defaultTitle: "CIPHERTron | Pritish Samal",
+  description: "Pritish Samal's Portfolio Website | CIPHERTron",
+  canonical: "https://sznm.dev",
+  openGraph: {
+    url: "https://pritishsamal.tech",
+    title: "Pritish Samal",
+    description: "Pritish Samal's Portfolio Website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/pritish007/image/upload/v1644348821/Personal%20Portfolio/Favicon_nckr8a.png",
+        alt: "sznm.dev og-image",
+      },
+    ],
+    site_name: "Pritish Samal",
+  },
+  twitter: {
+    handle: "@PritishSamal11",
+    cardType: "summary_large_image",
+  },
+};

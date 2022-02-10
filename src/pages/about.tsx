7d943808ca6cc1965ec0139fade567433266ca60
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:40:04 +0530

    add about and home route

---
+++ src/pages/about.tsx
@@ -0,0 +1,3 @@
+import About from "components/pages/about";
+
+export default About;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import Home from "components/pages/home";
+
+export { getStaticProps } from "components/pages/home/loader";
+export default Home;

b0f69e635a4856b35d293610bad181433f636a2b
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:37:23 +0530

    chore: add blog model

---
+++ src/models/blog.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+enum MandatoryFieldKeys {
+  id,
+  title,
+  date,
+  slug,
+  path,
+  type,
+  cover,
+  articleLink,
+}
+type MandatoryFieldsType = keyof typeof MandatoryFieldKeys;
+
+enum OptionalFieldKeys {
+  thumbnail,
+  description,
+}
+type OptionalFieldsType = keyof typeof OptionalFieldKeys;
+
+export type BlogPostType = Record<MandatoryFieldsType, string> &
+  Partial<Record<OptionalFieldsType, string>> & {
+    published: boolean;
+    contentHtml: any;
+    rawContent: any;
+    cover: any;
+    articleLink: any;
+    description?: string;
+    thumbnail?: string;
+    tags?: string[];
+  };

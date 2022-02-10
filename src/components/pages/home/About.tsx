0a04b8888cfca497b0d8829fef3d714a4f61b119
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:33:01 +0530

    feat: about component in homepage

---
+++ src/components/pages/home/About.tsx
@@ -0,0 +1,17 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+function About() {
+  return (
+    <Box>
+      <Heading size="lg">About Me</Heading>
+      <Text>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        provident in dolorum quae. Esse labore amet veritatis illo non porro?
+        Voluptatibus praesentium facilis vel dolor distinctio minus deserunt
+        consectetur? Repellat!
+      </Text>
+    </Box>
+  );
+}
+
+export default About;

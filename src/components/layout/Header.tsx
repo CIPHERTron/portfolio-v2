7da79a83850df5bc33996e1c670f219381d8a5f4
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:26:52 +0530

    feat: add header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,55 @@
+import { Box, Flex, Spacer, Text, useColorMode } from "@chakra-ui/react";
+import Link from "next/link";
+
+import Navigation from "./Navigation";
+import ThemeToggle from "./ThemeToggle";
+
+const Header = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      zIndex={5}
+      backgroundColor={
+        colorMode === "light"
+          ? "rgba(247, 250, 252, 0.8)"
+          : "rgba(26, 32, 44, 0.8)"
+      }
+      layerStyle="blur-bg"
+      width="full"
+    >
+      <Flex
+        layerStyle="layoutBlock"
+        marginX="auto"
+        maxWidth="800px"
+        as="header"
+        alignItems="center"
+      >
+        <Link href="/" passHref>
+          <Text
+            style={{ fontWeight: "bolder" }}
+            as="a"
+            cursor="pointer"
+            fontSize={["md", "xl"]}
+          >
+            PS.
+          </Text>
+        </Link>
+
+        <Spacer />
+
+        <Flex alignItems="center" gridGap={4}>
+          <Flex display={["none", "flex"]}>
+            <Navigation />
+          </Flex>
+
+          <ThemeToggle />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Header;

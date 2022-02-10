8b599891ff1b7571fd2b45f43dba76ab4258615a
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:26:37 +0530

    feat: add footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,27 @@
+import { Divider, Flex, Link, Stack, Text } from "@chakra-ui/react";
+
+import Links from "../pages/about/Links/index";
+
+const Footer = () => {
+  return (
+    <Stack as="footer" width="full" layerStyle="layoutBlock" spacing={6}>
+      <Divider />
+      <Flex justify="center">
+        <Links />
+      </Flex>
+
+      <Text align="center" fontSize={["xs", "sm"]}>
+        Built with <span style={{ marginRight: "5px" }}>💜</span> by{" "}
+        <Link
+          href="https://pritishsamal.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pritish Samal
+        </Link>
+      </Text>
+    </Stack>
+  );
+};
+
+export default Footer;

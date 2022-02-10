fa14a0f2eb1741099f2af530d8dae36b3ddecba7
CIPHERTron <pritish.samal918@gmail.com>
2022-02-10 17:32:34 +0530

    chore: skills and experience config

---
+++ src/config/experience.ts
@@ -0,0 +1,57 @@
+export const experience = [
+  {
+    org: "Kubernetes.io",
+    role: "Open Source Contributor",
+    link: "https://kubernetes.io/",
+    date: "September, 2021 - NOW",
+    content: [
+      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating",
+      "Helped solidify a brand direction for blistabloc that spans both packaging and web.",
+      "Interfaced with clients on a weekly basis, providing technological csqlb",
+    ],
+  },
+  {
+    org: "MLF Fellowship",
+    role: "Prep Fellow",
+    link: "https://fellowship.mlh.io/programs/prep",
+    date: "January, 2021 - February, 2021",
+    content: [
+      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating",
+      "Helped solidify a brand direction for blistabloc that spans both packaging and web.",
+      "Interfaced with clients on a weekly basis, providing technological ",
+    ],
+  },
+  {
+    org: "Cloud Native Computing Fdn.",
+    role: "SDE Intern",
+    link: "https://cncf.io",
+    date: "September, 2021 - November, 2021",
+    content: [
+      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from",
+      "Helped solidify a brand direction for blistabloc that spans both packaging and",
+      "Interfaced with clients on a weekly basis, providing technological",
+    ],
+  },
+  {
+    org: "Open Bioinformatics Fdn.",
+    role: "Contractor",
+    link: "https://www.open-bio.org/",
+    date: "June, 2021 - August, 2021",
+    content: [
+      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
+      "Helped solidify a brand direction for blistabloc that spans both packaging and web",
+      "Interfaced with clients on a weekly basis, providing technological expertise",
+    ],
+  },
+  {
+    org: "Humaps Private Ltd.",
+    role: "Freelance Developer",
+    link: "https://humaps.in",
+    date: "November, 2020 - December, 2020",
+    content: [
+      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
+      "Helped solidify a brand direction for blistabloc that spans both packaging and web",
+      "Interfaced with clients on a weekly basis, providing technological expertise",
+    ],
+  },
+];
---
+++ src/config/skills.ts
@@ -0,0 +1,122 @@
+export const skills = [
+  {
+    name: "React.js",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/react.svg",
+    type: "technology",
+  },
+  {
+    name: "Gatsby",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/gatsby.svg",
+    type: "technology",
+  },
+  {
+    name: "Next.js",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/nextjs.svg",
+    type: "technology",
+  },
+  {
+    name: "JavaScript",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/javascript.svg",
+    type: "language",
+  },
+  {
+    name: "TypeScript",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/typescript.svg",
+    type: "language",
+  },
+  {
+    name: "C++",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/cpp.svg",
+    type: "language",
+  },
+  {
+    name: "Python",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/python.svg",
+    type: "language",
+  },
+  {
+    name: "Golang",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/go.svg",
+    type: "language",
+  },
+  {
+    name: "Node.js",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/nodejs.svg",
+    type: "",
+  },
+  {
+    name: "Express",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/express.svg",
+    type: "technology",
+  },
+  {
+    name: "Flask",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/flask.svg",
+    type: "technology",
+  },
+  {
+    name: "Docker",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/docker.png",
+    type: "tool",
+  },
+  {
+    name: "Kubernetes",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/kubernetes.svg",
+    type: "tool",
+  },
+  {
+    name: "MongoDB",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/mongodb.svg",
+    type: "technology",
+  },
+  {
+    name: "SQLite",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/sqlite.svg",
+    type: "technology",
+  },
+  {
+    name: "Redux",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/redux.svg",
+    type: "technology",
+  },
+  {
+    name: "Firebase",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/firebase.svg",
+    type: "technology",
+  },
+  {
+    name: "Git",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/git.svg",
+    type: "technology",
+  },
+  {
+    name: "GitHub",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/github.svg",
+    type: "technology",
+  },
+  {
+    name: "Postman",
+    image:
+      "https://raw.githubusercontent.com/CIPHERTron/portfolio/main/public/images/skills/postman.svg",
+    type: "technology",
+  },
+];

70a23dd2d05bda380fa14bf474f92a9b666452ef
CIPHERTron <pritish.samal918@gmail.com>
2022-02-14 15:03:08 +0530

    chore: add experience content

--- src/config/experience.ts
+++ src/config/experience.ts
@@ -1,24 +1,13 @@
 export const experience = [
   {
-    org: "Kubernetes.io",
-    role: "Open Source Contributor",
-    link: "https://kubernetes.io/",
-    date: "September, 2021 - NOW",
-    content: [
-      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating",
-      "Helped solidify a brand direction for blistabloc that spans both packaging and web.",
-      "Interfaced with clients on a weekly basis, providing technological csqlb",
-    ],
-  },
-  {
     org: "MLF Fellowship",
     role: "MLH Prep Fellow",
     link: "https://fellowship.mlh.io/programs/prep",
     date: "January, 2021 - February, 2021",
     content: [
-      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating",
-      "Helped solidify a brand direction for blistabloc that spans both packaging and web.",
-      "Interfaced with clients on a weekly basis, providing technological ",
+      "A 3 week intensive program, collaborated with 15+ students from 4 different countries. Built out a portfolio of projects, experimented with new technologies during a short, impactful hackathon sprint.",
+      "Collaborated with the team with delegating tasks, conducting daily stand-ups, and reviewing PRs.",
+      "In addition to working on projects employing a variety of technologies such as Javascript, React, Jekyll, delivered a Show and Tell lecture on open-source at Kubernetes.",
     ],
   },
   {
@@ -27,9 +16,20 @@ export const experience = [
     link: "https://cncf.io",
     date: "September, 2021 - November, 2021",
     content: [
-      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from",
-      "Helped solidify a brand direction for blistabloc that spans both packaging and",
-      "Interfaced with clients on a weekly basis, providing technological",
+      "As a Linux Foundation Mentee, I worked on extending the test command of CNCF - Kyverno (a kubernetes native policy management engine) to cover generate policies",
+      "Drafted a structure of test.yaml for generate policies after multiple reviews and taking into consideration the existing test command workflow of the generate command.",
+      "Worked on extending the test command to cover generate policies using Go, kubernetes fake go-client, Docker and Kubernetes. Also wrote unit tests and e2e tests for the same.",
+    ],
+  },
+  {
+    org: "Kubernetes.io",
+    role: "Open Source Contributor",
+    link: "https://kubernetes.io/",
+    date: "September, 2021 - NOW",
+    content: [
+      "Apart from being an official member of Kubernetes, I was also a part of the structured logging migration Working Group where I migrated logs of kube-proxy from un-structured to structured logging.",
+      "I'm the reviewer of the kubernetes.dev site project where I worked on developing new features such as the events page, improving its SEO, triaging issues and PRs on a regular basis.",
+      "Worked on automating the process of uploading recordings from zoom cloud to YouTube using zapier and Golang",
     ],
   },
   {
@@ -38,9 +38,9 @@ export const experience = [
     link: "https://www.open-bio.org/",
     date: "June, 2021 - August, 2021",
     content: [
-      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
-      "Helped solidify a brand direction for blistabloc that spans both packaging and web",
-      "Interfaced with clients on a weekly basis, providing technological expertise",
+      "During Google Summer of Code, 2021 I worked as an external contractor at OBF to develop a journal policy tracker using python, poetry, & flask.",
+      "Used SQLite3 for storing data with SQLAlchemy as the ORM and automated its deployment to Heroku using GitHub actions. Later, containerized the application using Docker.",
+      "Attended weekly standups with my mentor as well as discussed and tweaked the database design to make it more scalable.",
     ],
   },
   {
@@ -49,9 +49,9 @@ export const experience = [
     link: "https://humaps.in",
     date: "November, 2020 - December, 2020",
     content: [
-      "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
-      "Helped solidify a brand direction for blistabloc that spans both packaging and web",
-      "Interfaced with clients on a weekly basis, providing technological expertise",
+      "Worked on bulding a highly scalable & responsive admin dashboard for an organization having more than 30 employees. Used React & Material UI for the frontend and Express, Mongo DB for the backend.",
+      "Built several functionalities starting from Auth0 based authentication, order booking system, realtime notification, statistics in-terms of graphical representation and many more.",
+      "Finally, used concurrently package to spin both the client and server with a single command and deployed it to heroku.",
     ],
   },
 ];

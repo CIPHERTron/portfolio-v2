dd75fbcb5b8f3ba3706e1d7f90c40b8df2286fd9
CIPHERTron <pritish.samal918@gmail.com>
2022-03-07 01:21:12 +0530

    feat: persist active state of nav item

--- src/components/layout/Navbar/navconfig.ts
+++ src/components/layout/Navbar/navconfig.ts
@@ -2,21 +2,26 @@ export const navigations = [
   {
     href: "/",
     label: "Home",
+    active: "",
   },
   {
     href: "/projects",
     label: "Projects",
+    active: "projects",
   },
   {
     href: "/blog",
     label: "Blog",
+    active: "blog",
   },
   {
     href: "/about",
     label: "About",
+    active: "about",
   },
   {
     href: "/sessions",
     label: "Sessions",
+    active: "sessions",
   },
 ];

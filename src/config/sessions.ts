b32d30e6226ce3ce96efbdc2842b191239c13053
Pritish Samal <pritish.samal918@gmail.com>
2023-08-03 18:14:05 +0530

    feat: add latest session (#59)
    * feat: add latest session
    
    * Update sessions.ts

--- src/config/sessions.ts
+++ src/config/sessions.ts
@@ -1,16 +1,25 @@
 export const sessions = [
   {
+    title: "LFX Mentorship",
+    date: "24 January 2023",
+    image:
+      "https://res.cloudinary.com/pritish007/image/upload/v1691065873/Personal%20Portfolio/Sessions/lfx-aniket.jpg",
+    desc: "OpenSource is life-changing for a lot of folks. Companies are hiring OpenSource Engineers. LFX Mentorship is for folks starting out in OpenSource and interested in exploring the modern tech niches companies are looking forward to. The infamous Google Summer of Code is a well-known program...",
+    rec: "https://youtu.be/o0V7Z6UQMBY",
+    slides: "https://www.youtube.com/@palaniket",
+  },
+  {
     title: "All about LFX Mentorship",
-    date: "9 June, 2022",
+    date: "9 June 2022",
     image:
       "https://res.cloudinary.com/pritish007/image/upload/v1654950223/Personal%20Portfolio/Sessions/lfxsession_tfxoap.jpg",
-    desc: "This session was organized by the Community Classroom where we talked about the LFX Mentorship Program by the Linux Foundation, the application procedure, eligibility criteria and some tips & tricks for applicants. We also had an AMA session at the end to clear the genral queries of students.",
+    desc: "This session was organized by the Community Classroom where we talked about the LFX Mentorship Program by the Linux Foundation, the application procedure, eligibility criteria and some tips & tricks for applicants. We also had an AMA session at the end to clear the general queries of students.",
     rec: "https://youtu.be/YhivM-V_Pgk",
     slides: "https://lfx.linuxfoundation.org/tools/mentorship/",
   },
   {
     title: "Introduction to Open Source",
-    date: "11 February, 2022",
+    date: "11 February 2022",
     image:
       "https://res.cloudinary.com/pritish007/image/upload/v1645039838/Personal%20Portfolio/Sessions/rjit_iguky5.jpg",
     desc: "This session was organized by GirlScript Chapter RJIT, one of India's leading foundations which leaves no stones unturned in helping budding geeks to kickstart their journey with open source. Here I spoke about a beginners journey to get started with contributing to open-source and basics of git & GitHub.",
@@ -20,7 +29,7 @@ export const sessions = [
   },
   {
     title: "Beginner's Guide to React & CSS-in-JS Libraries",
-    date: "26 January, 2022",
+    date: "26 January 2022",
     image:
       "https://res.cloudinary.com/pritish007/image/upload/v1645039838/Personal%20Portfolio/Sessions/juet_lai79u.jpg",
     desc: "This session was organized by a local community in JUET where I delivered a hands-on-workshop on basics of React, JSX, components & props, state & lifecycle events and how to use css-in-js libraries like styled components with React.",
@@ -30,20 +39,20 @@ export const sessions = [
   },
   {
     title: "LFX Mentorship Program",
-    date: "11 February, 2022",
+    date: "11 February 2022",
     image:
       "https://res.cloudinary.com/pritish007/image/upload/v1645039839/Personal%20Portfolio/Sessions/codxcrypt_jucvcc.jpg",
     desc: "This session was organized by the CodXCrypt Community where we talked about the LFX Mentorship Program by the Linux Foundation. We talked about the application procedure, eligibility criteria and some tips & tricks for students to make their application stand out.",
     rec: "https://youtu.be/a8PKrnLo91A",
     slides: "https://lfx.linuxfoundation.org/tools/mentorship/",
   },
-  // {
-  //   title: "Decoding Coding",
-  //   date: "18 December, 2020",
-  //   image:
-  //     "https://res.cloudinary.com/pritish007/image/upload/v1645039838/Personal%20Portfolio/Sessions/dscnitr_g8gibo.jpg",
-  //   desc: "This session was organized by GDSC NIT Rourkela. It was basically an AMA session where we answered the general queries of beginners who want to start their coding journey",
-  //   rec: "https://youtube.com/playlist?list=PLxK2vL6QW00d0g1GP4sg4ClIAtRfOPaDv",
-  //   slides: "https://dscnitrourkela.org/",
-  // },
+  {
+    title: "Decoding Coding",
+    date: "18 December 2020",
+    image:
+      "https://res.cloudinary.com/pritish007/image/upload/v1645039838/Personal%20Portfolio/Sessions/dscnitr_g8gibo.jpg",
+    desc: "GDSC NIT Rourkela organized this session. It was basically an AMA session where we answered the general queries of beginners who want to start their coding journey",
+    rec: "https://youtube.com/playlist?list=PLxK2vL6QW00d0g1GP4sg4ClIAtRfOPaDv",
+    slides: "https://dscnitrourkela.org/",
+  },
 ];
